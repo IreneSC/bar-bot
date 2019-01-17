332b9e9fa3210cd37a54cc3cb4a36a13c9350f95
Brendan-Hollaway <brendan.j.hollaway@gmail.com>
2019-01-16 22:19:50 -0800

    Add code for PS1 Problem 7, Simple Moving.

---
+++ sinusoidal_motion.m
@@ -0,0 +1,53 @@
+% Send position commands, log in the background, and plot offline.
+%
+% For more information type:
+%    help CommandStruct
+%    help HebiGroup
+%
+% This script assumes you can create a group with 1 module.
+%
+% HEBI Robotics
+% June 2018
+
+%% Setup
+clear *;
+close all;
+HebiLookup.initialize();
+
+familyName = 'Arm';
+moduleNames = 'tapedispenser';  
+group = HebiLookup.newGroupFromNames( familyName, moduleNames );
+
+%% Open-Loop Controller (Position)
+% The command struct has fields for position, velocity, and effort.  
+% Fields that are empty [] or NaN will be ignored when sending.
+cmd = CommandStruct(); 
+
+% Starts logging in the background
+group.startLog( 'dir', 'logs' );  
+
+% Parameters for sin/cos function
+freqHz = 1;           % [Hz]
+freq = freqHz * 2*pi;   % [rad / sec]
+amp = deg2rad( 90 );    % [rad]
+
+original_position = group.getNextFeedback().position;
+start_angle = deg2rad(90);
+
+duration = 10; % [sec]
+timer = tic();
+while toc(timer) < duration
+    
+    % Even though we don't use the feedback, getting feedback conveniently 
+    % limits the loop rate to the feedback frequency
+    fbk = group.getNextFeedback();  
+
+    % Update position set point
+    cmd.position = (original_position - amp) + amp * sin( freq * toc(timer) + start_angle);   
+    group.send(cmd); 
+    
+end
+
+% Stop logging and plot the position data using helper functions
+log = group.stopLog();
+HebiUtils.plotLogs( log, 'position' );
